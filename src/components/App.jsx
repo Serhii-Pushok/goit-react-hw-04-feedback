b12812ae04e58b76c5b784e762ba6b6e326b96fa
Serhii Pushok <sergeypushok1991@gmail.com>
2023-02-22 21:03:42 +0200

    add useState

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,63 @@
+import { useState } from "react";
+import { Statistics } from "./Statistics/Statistics";
+import { FeedbackOptions } from "./FeedbackOptions/FeedbackOptions";
+import { Section } from "./Section/Section";
+import { Notification } from "./Notofication/Notification";
+import css from "./App.module.css";
+
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+
+    const [good, setGood] = useState(0);
+    const [neutral, setNeutral] = useState(0);
+    const [bad, setBad] = useState(0);
+ 
+
+    const handleClick = event => {
+        const { name } = event.target;
+        
+        switch (name) {
+            case "good":
+                setGood(prevState => prevState + 1)
+                break;
+            case "neutral":
+                setNeutral(prevState => prevState + 1)
+                break;
+            case "bad":
+                setBad(prevState => prevState + 1)
+                break;
+            
+            default:
+                return;
+        }
+  }
+
+    const countTotalFeedback = () => {
+      return good + neutral + bad;
+  }
+        
+    const countPositiveFeedbackPercentage = () => {
+      return Math.floor(good / (good + neutral + bad) * 100) + '%'
+  }
+
+      return (
+          <div className={css.container}>
+              <Section title="Please leave feedback">
+                  <FeedbackOptions options={["good", "bad", "neutral"]} onLeaveFeedback={handleClick}/>
+              </Section>
+
+              <Section title="Statistics">
+                  {countTotalFeedback() >= 1
+                  ?
+                  <Statistics
+                  good={good}
+                  neutral={neutral}
+                  bad={bad}
+                  total={countTotalFeedback}
+                  positivePercentage={countPositiveFeedbackPercentage} />
+                  :
+                  <Notification message="There is no feedback" />}
+              </Section>
+          </div>
+      )
+   
 };
